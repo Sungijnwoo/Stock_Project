c361d76bb9def5e0bc08cee71b8b10d1a1116ac6
JHyun96 <jonghyun9606@gmail.com>
2021-03-30 21:14:34 +0900

    ADD Days_Price.py

--- AutoTrade/Days_Price.py
+++ AutoTrade/Days_Price.py
@@ -1,5 +1,15 @@
 import win32com.client
- 
+import pandas as pd
+import os 
+
+day_list = []
+time_list = []
+open_list = []
+high_list = []
+low_list = []
+close_list = []
+vol_list = []
+amount_list = []
 # 연결 여부 체크
 objCpCybos = win32com.client.Dispatch("CpUtil.CpCybos")
 bConnect = objCpCybos.IsConnect
@@ -9,24 +19,45 @@ if (bConnect == 0):
  
 # 차트 객체 구하기
 objStockChart = win32com.client.Dispatch("CpSysDib.StockChart")
-objStockChart.SetInputValue(0, 'A011040')   #종목 코드 - 삼성전자
+objStockChart.SetInputValue(0, 'A075970')   #종목 코드 - 동국알앤에스
 objStockChart.SetInputValue(1, ord('2')) # 개수로 조회
-objStockChart.SetInputValue(4, 100) # 최근 100일 치
-objStockChart.SetInputValue(5, [0,2,3,4,5, 8]) #날짜,시가,고가,저가,종가,거래량
-objStockChart.SetInputValue(6, ord('D')) # '차트 주가 - 일간 차트 요청
+objStockChart.SetInputValue(4, 382) # 하루 381개 0900 ~ 1520
+objStockChart.SetInputValue(5, [0,1,2,3,4,5, 8,9]) #날짜,시간,시가,고가,저가,종가,거래량,거래대금
+objStockChart.SetInputValue(6, ord('m')) # '차트 주가 - 일간 차트 요청
+objStockChart.SetInputValue(7, 5)
 objStockChart.SetInputValue(9, ord('1')) # 수정주가 사용
 objStockChart.BlockRequest()
  
 len = objStockChart.GetHeaderValue(3)
  
-print("날짜", "시가", "고가", "저가", "종가", "거래량")
+print("날짜","시간", "시가", "고가", "저가", "종가", "거래량","거래대금")
 print("빼기빼기==============================================-")
 
 for i in range(len):
-    day = objStockChart.GetDataValue(0, i)
-    open = objStockChart.GetDataValue(1, i)
-    high = objStockChart.GetDataValue(2, i)
-    low = objStockChart.GetDataValue(3, i)
-    close = objStockChart.GetDataValue(4, i)
-    vol = objStockChart.GetDataValue(5, i)
-    print (day, open, high, low, close, vol)
+    # day = objStockChart.GetDataValue(0, i)
+    # time = objStockChart.GetDataValue(1, i)
+    # open = objStockChart.GetDataValue(2, i)
+    # high = objStockChart.GetDataValue(3, i)
+    # low = objStockChart.GetDataValue(4, i)
+    # close = objStockChart.GetDataValue(5, i)
+    # vol = objStockChart.GetDataValue(6, i)
+    # amount = objStockChart.GetDataValue(7, i)
+    # print (day, time, open, high, low, close, vol,amount)
+
+    day_list.append(objStockChart.GetDataValue(0, i))
+    time_list.append(objStockChart.GetDataValue(1, i))
+    open_list.append(objStockChart.GetDataValue(2, i))
+    high_list.append(objStockChart.GetDataValue(3, i))
+    low_list.append(objStockChart.GetDataValue(4, i))
+    close_list.append(objStockChart.GetDataValue(5, i))
+    vol_list.append(objStockChart.GetDataValue(6, i))
+    amount_list.append(objStockChart.GetDataValue(7, i))
+dict1 = {'day' : day_list, 'time' : time_list, 'open' : open_list, 'high' : high_list, 'low' : low_list, \
+'close' : close_list, 'vol' : vol_list, 'amount' : amount_list}
+
+df = pd.DataFrame(dict1, columns=['day','time','open','high','low','close','vol','amount'])
+df.sort_index(ascending=False)
+print(df)
+folder_path = os.getcwd()
+df.to_excel('stock_datacsv')
+os.startfile(folder_path)
