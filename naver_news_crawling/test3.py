7670b5acd4b14e8ae08e2c3d614a4ff5ebfc1e9e
jinwoo <tjdwnsdudd3@naver.com>
2021-04-04 13:05:50 +0900

    EXTRACT news_time

--- naver_news_crawling/test3.py
+++ naver_news_crawling/test3.py
@@ -1,6 +1,40 @@
 import pandas as pd
+import os
 
-a = pd.read_excel("code_KOSDAQ.xlsx")
-b = a.loc[0:2, 'name']
-for i in b:
-    print(i)
+news = ["라온시큐어", "알로이스"]
+
+# for new in news:
+#     a = pd.read_excel("네이버뉴스_본문_1000개_{}.xlsx".format(new))
+#     for x, i in enumerate(a['time']):
+#         for y, j in enumerate(i):
+#             if j.isdigit():
+#                 a['time'][x] = i[y:y+8]
+#                 break
+
+# for new in news:
+#     a = pd.read_excel("네이버뉴스_본문_1000개_{}.xlsx".format(new))
+#     for x, i in enumerate(a['time']):
+#         try:
+#             if len(str(i)) > 8:
+#                 a['time'][x] = str(i)[:8]
+#         except:
+#             pass
+
+for new in news:
+    a = pd.read_excel("네이버뉴스_본문_1000개_{}.xlsx".format(new))
+    idx_num_l = []
+    for x, i in enumerate(a['time']):
+        try:
+            if not i.isdigit():
+                idx_num_l += x,
+        except:
+            pass
+    a = a.drop(idx_num_l)
+            
+    folder_path = os.getcwd()
+    xlsx_file_name = '네이버뉴스_본문_1000개_{}.xlsx'.format(new)
+    a.to_excel(xlsx_file_name, index=False)
+
+    print('엑셀 저장 완료 | 경로 : {}\\{}\n'.format(folder_path, xlsx_file_name))
+
+    os.startfile(folder_path)
